2145582d71adaa1f7f5b8951a4bd41e4ec4e18a0
Candace <goodsoncj@gmail.com>
2017-08-30 23:56:27 +0000

    added bootstrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,5 +50,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  #8/30/2017 - candace commented out below per mentor instruction to avoid frequent rail server reboots
+  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
